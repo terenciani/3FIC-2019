4ed30c1492003e5225cbb21021806d61c6a0021c
Marcelo F. Terenciani <marcelo.figterenciani@gmail.com>
2019-05-14 10:47:49 -0400

    atualização do projeto

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-app>
+    <v-content>
+      <v-toolbar color="red" dark>
+        <v-toolbar-side-icon color="white--text"></v-toolbar-side-icon>
+        <v-toolbar-title>Barberia</v-toolbar-title>
+        <v-spacer></v-spacer>
+        <v-text-field label="Pesquisar" append-icon="search"></v-text-field>
+        <v-spacer></v-spacer>
+        <v-toolbar-items class="hidden-sm-and-down">
+          <v-btn flat>Início</v-btn>
+          <v-btn flat>Produtos</v-btn>
+          <v-btn flat>Contato</v-btn>
+        </v-toolbar-items>
+      </v-toolbar>
+    </v-content>
+  </v-app>
+</template>
